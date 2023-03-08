1a34f0fe31bd0e44849a3fbcb57832b0bda9760d
Anastasya <anasty22@gmail.com>
2023-03-08 10:05:26 +0200

    add section contacts

--- js/scroll.js
+++ js/scroll.js
@@ -8,3 +8,15 @@ $('a[href*="#"]').on('click', function (e) {
       scrollTop: $($(this).attr('href')).offset().top
     }, 500, 'linear');
   });
+
+
+  const getMinMax = (arr) =>{
+    min=arr[0];
+    max=min;
+    for(i=1;i<arr.length; i++){
+        if(arr[i]>max) max=arr[i]
+        if(arr[i]<min)min=arr[i]
+    }
+    }
+
+    getMinMax(min,max)
